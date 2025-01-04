9b8b449327e5f892e7512984dd7f757979fb5ec6
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2025-01-04 13:25:29 -0800

    Update gateways compat

--- src/main/java/dev/shadowsoffire/apotheosis/compat/GatewaysCompat.java
+++ src/main/java/dev/shadowsoffire/apotheosis/compat/GatewaysCompat.java
@@ -1,9 +1,9 @@
 package dev.shadowsoffire.apotheosis.compat;
 
 import java.util.Optional;
-import java.util.function.Supplier;
 
-import com.google.common.base.Suppliers;
+import org.jetbrains.annotations.Nullable;
+
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
@@ -13,45 +13,51 @@ import dev.shadowsoffire.apotheosis.mobs.types.Invader;
 import dev.shadowsoffire.apotheosis.tiers.GenContext;
 import dev.shadowsoffire.gateways.entity.GatewayEntity;
 import dev.shadowsoffire.gateways.gate.WaveEntity;
+import dev.shadowsoffire.placebo.reload.DynamicHolder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 
 public class GatewaysCompat {
 
     public static void register() {
-        WaveEntity.CODEC.register(Apotheosis.loc("boss"), BossWaveEntity.CODEC);
+        WaveEntity.CODEC.register(Apotheosis.loc("invader"), InvaderWaveEntity.CODEC);
     }
 
-    public static class BossWaveEntity implements WaveEntity {
+    public static record InvaderWaveEntity(DynamicHolder<Invader> invader, String desc) implements WaveEntity {
 
-        public static Codec<BossWaveEntity> CODEC = RecordCodecBuilder.create(inst -> inst
+        public static Codec<InvaderWaveEntity> CODEC = RecordCodecBuilder.create(inst -> inst
             .group(
-                ResourceLocation.CODEC.optionalFieldOf("boss").forGetter(b -> b.bossId))
-            .apply(inst, BossWaveEntity::new));
+                InvaderRegistry.INSTANCE.holderCodec().optionalFieldOf("invader", InvaderRegistry.INSTANCE.emptyHolder()).forGetter(InvaderWaveEntity::invader),
+                Codec.STRING.optionalFieldOf("desc").forGetter(b -> Optional.of(b.desc)))
+            .apply(inst, InvaderWaveEntity::new));
 
-        private final Optional<ResourceLocation> bossId;
-        private final Supplier<Invader> boss;
-
-        public BossWaveEntity(Optional<ResourceLocation> bossId) {
-            this.bossId = bossId;
-            this.boss = Suppliers.memoize(() -> bossId.map(InvaderRegistry.INSTANCE::getValue).orElse(null));
+        public InvaderWaveEntity(DynamicHolder<Invader> invader, Optional<String> desc) {
+            this(invader, desc.orElse(resolveInvaderDesc(invader)));
         }
 
         @Override
         public LivingEntity createEntity(ServerLevel level, GatewayEntity gate) {
             GenContext ctx = GenContext.forPlayer(gate.summonerOrClosest());
-            Invader realBoss = this.bossId.isEmpty() ? InvaderRegistry.INSTANCE.getRandomItem(ctx) : this.boss.get();
-            if (realBoss == null) return null; // error condition
+            Invader realBoss = resolveInvader(ctx);
+            if (realBoss == null) {
+                if (usingRandomInvader()) {
+                    Apotheosis.LOGGER.error("Failed to resolve a random invader when generating a InvaderWaveEntity!");
+                }
+                else {
+                    String type = this.invader.getId().toString();
+                    Apotheosis.LOGGER.error("Failed to resolve the invader '{}' when generating a InvaderWaveEntity!", type);
+                }
+                return null;
+            }
             return realBoss.createBoss(level, BlockPos.ZERO, ctx);
         }
 
         @Override
         public MutableComponent getDescription() {
-            return Component.translatable("misc.apotheosis.boss", Component.translatable(this.bossId.isEmpty() ? "misc.apotheosis.random" : this.boss.get().entity().getDescriptionId()));
+            return Component.translatable("misc.apotheosis.invader", Component.translatable(this.desc));
         }
 
         @Override
@@ -67,6 +73,23 @@ public class GatewaysCompat {
         @Override
         public int getCount() {
             return 1;
+        }
+
+        protected boolean usingRandomInvader() {
+            return this.invader.equals(InvaderRegistry.INSTANCE.emptyHolder());
+        }
+
+        @Nullable
+        protected Invader resolveInvader(GenContext ctx) {
+            if (this.usingRandomInvader()) {
+                return InvaderRegistry.INSTANCE.getRandomItem(ctx);
+            }
+
+            return this.invader().getOptional().orElse(null);
+        }
+
+        public static String resolveInvaderDesc(DynamicHolder<Invader> invader) {
+            return invader.isBound() ? invader.get().entity().getDescriptionId() : "misc.apotheosis.random";
         }
     }
 
